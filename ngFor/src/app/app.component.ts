08b3a825f806ff43fe9ab2d784dcb1dd05c91617
Fco Jesus Díaz Rubio <fjdiazr@gmail.com>
2017-01-26 06:51:09 +0100

    ngFor

---
+++ ngFor/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+ 
+ public tipos: any[] = [
+    { id: 1, text: "Ingreso" },
+    { id: 2, text: "Gasto" },
+    { id: 3, text: "Expediente" }
+  ];
+
+}
